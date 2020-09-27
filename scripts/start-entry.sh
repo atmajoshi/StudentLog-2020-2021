b6b7ef5c9756ade3eba5f2b473945fd85895bb13
Atma <ajoshi2023@chadwickschool.org>
2020-09-27 11:28:59 -0700

    Finished my script

--- scripts/start-entry.sh
+++ scripts/start-entry.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -e
 
-$FILENAME="logs/$(date +'%Y-%m-%d').log.md"
+FILENAME="logs/$(date +'%Y-%m-%d').log.md"
+code "$FILENAME"
+touch $FILENAME
+echo "# $(date +'%A, %B %e, %Y %I:%M %p')" >> $FILENAME
+echo "" >> $FILENAME
+echo "- [ ]" >> $FILENAME
